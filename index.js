cfbce6093e1d82d1d607b4aeeac450b5d906d52b
Daisy Jimenez <jimenez.daisy9494@gmail.com>
2024-11-27 15:19:03 -0800

    objects lab done

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+const employee = {
+    name: "Disney",
+    streetAddress: "1234 Disneyland"
+}
+
+function updateEmployeeWithKeyAndValue( empObj,  key, value) {
+    const newObj = { ...empObj }
+    newObj[key] = value
+
+    return newObj
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue( empObj,  key, value) {
+    empObj[key] = value
+
+    return empObj
+} 
+
+function deleteFromEmployeeByKey( empObj,  key, value) {
+    const newObj = { ...empObj }
+
+    delete newObj[key]
+
+    return newObj
+} 
+
+function destructivelyDeleteFromEmployeeByKey( empObj,  key, value) {
+    
+    delete empObj[key]
+    
+    return empObj
+}
